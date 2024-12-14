410dd9d2dbb66c0c48f7e03e8ec8b49639784f09
avishkarlakade <avishkarlakade74@gmail.com>
2024-12-14 15:45:43 +0530

    first

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Chatbox from './Chatbox'
 function App() {
- const api_key =  import.meta.env.VITE_API_KEY;
+ const api_key = "AIzaSyABFpqT4IkhVc6VDo6tM-S4CU2mMYqZnI0"
 
   return (
     <Chatbox api_key={api_key}/>
